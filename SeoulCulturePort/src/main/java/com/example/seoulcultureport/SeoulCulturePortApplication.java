7e171664b97bf5efa3b0b92532837eb8d81f42ac
jangjinhyeok <jangjh45@naver.com>
2023-03-17 17:26:54 +0900

    feat: 시작

---
+++ SeoulCulturePort/src/main/java/com/example/seoulcultureport/SeoulCulturePortApplication.java
@@ -0,0 +1,13 @@
+package com.example.seoulcultureport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulCulturePortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeoulCulturePortApplication.class, args);
+    }
+
+}
---
+++ SeoulCulturePort/src/test/java/com/example/seoulcultureport/SeoulCulturePortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.seoulcultureport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulCulturePortApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
