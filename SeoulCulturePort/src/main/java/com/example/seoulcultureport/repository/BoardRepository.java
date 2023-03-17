75c6869ff4fecb66d086decca2072263e7a78ae0
bambee <daj0909@naver.com>
2023-03-17 19:03:17 +0900

    gitignore

---
+++ SeoulCulturePort/src/main/java/com/example/seoulcultureport/repository/BoardRepository.java
@@ -0,0 +1,4 @@
+package com.example.seoulcultureport.repository;
+
+public interface BoardRepository {
+}
---
+++ SeoulCulturePort/src/main/java/com/example/seoulcultureport/service/BoardService.java
@@ -0,0 +1,4 @@
+package com.example.seoulcultureport.service;
+
+public class BoardService {
+}
