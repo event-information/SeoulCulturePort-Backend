07ce91e204f37eeff0ccf0264ebc7b1a0359f4fd
bambee <daj0909@naver.com>
2023-03-18 16:31:36 +0900

    boardconfig 변경 (게시글상세페이지조회permitall)

--- SeoulCulturePort/src/main/java/com/example/seoulcultureport/config/WebSecurityConfig.java
+++ SeoulCulturePort/src/main/java/com/example/seoulcultureport/config/WebSecurityConfig.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -54,6 +55,7 @@ public class WebSecurityConfig {
         http.authorizeRequests()
                 .antMatchers("/signup", "/login", "/check").permitAll()
                 .antMatchers("/api/user/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/board/detail/**").permitAll()
                 .anyRequest().authenticated()
                 .and().addFilterBefore(new JwtAuthFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
