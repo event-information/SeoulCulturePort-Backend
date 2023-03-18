838e9f6dc0e8a6581f57e9c36758c37484ce8f43
ㅡApfelsaft-ASAP <myoungbeen@gmail.com>
2023-03-18 11:06:01 +0800

    CommentService updateComment 반환 메세지 수정#1

--- SeoulCulturePort/src/main/java/com/example/seoulcultureport/service/CommentService.java
+++ SeoulCulturePort/src/main/java/com/example/seoulcultureport/service/CommentService.java
@@ -7,6 +7,7 @@ import com.example.seoulcultureport.entity.User;
 import com.example.seoulcultureport.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -14,16 +15,18 @@ public class CommentService {
 
     private final CommentRepository commentRepository;
 
-    public CommentResponseDto updateComment(Long id, CommentRequestDto commentRequestDto, User user) {
+    @Transactional
+    public MessageResponseDto updateComment(Long id, CommentRequestDto commentRequestDto, User user) {
         Comment comment = commentRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("")
         );
         if(user.getUsername().equals(comment.getUsername())) {
             comment.update(commentRequestDto);
+            commentRepository.saver(comment);
         }
         else{
             throw new IllegalArgumentException("");
         }
-        return new CommentResponseDto(comment);
+        return new MessageResponseDto(StatusEnum.OK);
     }
 }
