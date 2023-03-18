008e534c864bfac772df6c9464a862d5afd730f5
bambee <daj0909@naver.com>
2023-03-18 11:01:44 +0900

    1차완성

--- SeoulCulturePort/src/main/java/com/example/seoulcultureport/controller/BoardController.java
+++ SeoulCulturePort/src/main/java/com/example/seoulcultureport/controller/BoardController.java
@@ -3,10 +3,11 @@ package com.example.seoulcultureport.controller;
 import com.example.seoulcultureport.dto.*;
 import com.example.seoulcultureport.service.BoardService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+
+//반환타입 Enum 으로 바꾸거나 Exception 처리하기 !
 
 @RestController @RequiredArgsConstructor
 @RequestMapping("/api/board")
@@ -17,43 +18,43 @@ public class BoardController {
     @PostMapping("/")
     public ResponseEntity<BoardResponseDto> writeBoard(@RequestBody BoardRequestDto boardRequestDto, @AuthenticationPrincipal UserDetailsImpl userDetails) {
         BoardDetailResponseDto response = boardService.writeBoard(boardRequestDto, userDetails.getUser());
-        return new ResponseEntity.ok(new BoardResponseDto (200,"success"));
+        return ResponseEntity.ok(new BoardResponseDto(200, "success"));
     }
 
     // 내 게시글 수정
      @PutMapping("/{id}")
         public ResponseEntity<BoardResponseDto> updateBoard(@PathVariable Long id, @RequestBody BoardRequestDto boardRequest, @AuthenticationPrincipal UserDetailsImpl userDetails){
          BoardDetailResponseDto response = boardService.updateBoard(id, boardRequest, userDetails.getUser());
-            return new ResponseEntity.ok(new BoardResponseDto(200,"success"));  //Enum으로 ㅅ쓰세요 !!
+            return ResponseEntity.ok(new BoardResponseDto(200,"success"));
         }
 
     // 내 게시글 삭제
       @DeleteMapping("/{id}")
         public ResponseEntity<BoardResponseDto> deleteBoard(@PathVariable Long id, @AuthenticationPrincipal UserDetailsImpl userDetails) {
             boardService.deleteBoard(id, userDetails.getUser());
-            return new ResponseEntity.ok(new BoardResponseDto(200,"success"));
+            return ResponseEntity.ok(new BoardResponseDto(200,"success"));
         }
 
-    //메인페이지 전체 글 리스트 조회
+    //메인페이지 전체 글 [리스트] 조회 (토큰x)
         @GetMapping("/list")
         public ResponseEntity<BoardResponseDto> getBoardList() {
             List<BoardListResponseDto> response = boardService.getBoardList();
-            return new ResponseEntity.ok(new BoardResponseDto(200,"success"));
+            return ResponseEntity.ok(new BoardResponseDto(200,"success"));
         }
 
-    // 상세페이지
+    // 상세페이지 (토큰 x)
         @GetMapping("/detail/{id}")
         public ResponseEntity<BoardResponseDto> getBoardDetailList(@PathVariable Long id) {
             BoardDetailResponseDto response = boardService.getBoardDetailList(id);
-            return new  ResponseEntity.ok(new BoardResponseDto(200,"success"));
+            return ResponseEntity.ok(new BoardResponseDto(200,"success"));
         }
 
 
-    // 내 게시글 리스트
+    // 내 게시글 [리스트] (토큰 o)
         @GetMapping("/mylist")
         public ResponseEntity<BoardResponseDto> getBoardMyList(@AuthenticationPrincipal UserDetailsImpl userDetails) {
             List<BoardSimpleResponseDto> response = boardService.getBoardMyList(userDetails.getUser());
-            return new ResponseEntity.ok(new BoardResponseDto(200,"success"));
+            return ResponseEntity.ok(new BoardResponseDto(200,"success"));
         }
 
 
