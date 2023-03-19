1abe7b1549b05e21b4261a64bb0c316dbea50f89
jangjinhyeok <jangjh45@naver.com>
2023-03-19 14:29:48 +0900

    fix: 예외처리 코드 수정

--- SeoulCulturePort/src/main/java/com/example/seoulcultureport/dto/PatchNickRequestDto.java
+++ SeoulCulturePort/src/main/java/com/example/seoulcultureport/dto/PatchNickRequestDto.java
@@ -3,13 +3,14 @@ package com.example.seoulcultureport.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class PatchNickRequestDto {
 
-    @NotNull(message = "닉네임은 공백으로 둘 수 없습니다.")
+    @NotBlank(message = "닉네임은 공백으로 둘 수 없습니다.")
     private String nickname;
 
 }
