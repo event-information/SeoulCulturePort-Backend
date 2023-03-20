e20ad4d261b0af3a6644422f5398b7854c52fa95
jangjinhyeok <jangjh45@naver.com>
2023-03-21 01:05:58 +0900

    fix: 자동배포 수정

---
+++ scripts/stop.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+PROJECT_ROOT="/home/ubuntu/app"
+JAR_FILE="$PROJECT_ROOT/SeoulCulturePort-0.0.1-SNAPSHOT.jar"
+
+DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+
+TIME_NOW=$(date +%c)
+
+# 현재 구동 중인 애플리케이션 pid 확인
+CURRENT_PID=$(pgrep -f $JAR_FILE)
+
+# 프로세스가 켜져 있으면 종료
+if [ -z $CURRENT_PID ]; then
+  echo "$TIME_NOW > 현재 실행중인 애플리케이션이 없습니다" >> $DEPLOY_LOG
+else
+  echo "$TIME_NOW > 실행중인 $CURRENT_PID 애플리케이션 종료 " >> $DEPLOY_LOG
+  kill -9 $CURRENT_PID
+fi
+
+# 애플리케이션이 완전히 종료될 때까지 대기
+while [ -n $CURRENT_PID ]
+do
+  sleep 1
+  CURRENT_PID=$(pgrep -f $JAR_FILE)
+done
