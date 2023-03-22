350114cd484cab22ec35281df4fd65c54716784f
jangjinhyeok <jangjh45@naver.com>
2023-03-22 23:53:53 +0900

    fix: 댓글시간 초만 나오게 수정

--- src/main/java/com/example/seoulcultureport/dto/commentDto/CommentResponseDto.java
+++ src/main/java/com/example/seoulcultureport/dto/commentDto/CommentResponseDto.java
@@ -6,6 +6,7 @@ import com.example.seoulcultureport.entity.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 
 @Getter
@@ -15,18 +16,17 @@ public class CommentResponseDto {
     private Long id;
     private String comment;
     private String nickname;
-    private String createdat;
+    private Long createdat;
     private int thumbsUpCount;
 
     private boolean commentThumbsupStatus;
 
     public CommentResponseDto(Comment comment, User user) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         this.id = comment.getId();
         this.comment = comment.getComment();
         this.thumbsUpCount = comment.getCommentLikes().size();
         this.nickname = comment.getNickname();
-        this.createdat = comment.getCreatedAt().format(formatter);
+        this.createdat = comment.getCreatedAt().toInstant(ZoneOffset.UTC).getEpochSecond();
         if(user != null) {
             this.commentThumbsupStatus = comment.commentThumbsupByUser(user.getId());
         } else {
